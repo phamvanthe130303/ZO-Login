44ecefab59ca2a7f8c66c0db56701499faa04ddb
Thể <thepv13032003@fpt.edu.vn>
2024-11-12 10:30:28 +0700

    create project

---
+++ ZO-Login/src/main/java/com/example/zologin/ZoLoginApplication.java
@@ -0,0 +1,13 @@
+package com.example.zologin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZoLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZoLoginApplication.class, args);
+	}
+
+}
---
+++ ZO-Login/src/test/java/com/example/zologin/ZoLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zologin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZoLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
