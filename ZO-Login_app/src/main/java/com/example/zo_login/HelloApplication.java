7e251ae30fecfed93588d4beeb894f0c6f68dae8
Thể <thepv13032003@fpt.edu.vn>
2024-11-21 17:29:04 +0700

    update

--- ZO-Login_app/src/main/java/com/example/zo_login/HelloApplication.java
+++ ZO-Login_app/src/main/java/com/example/zo_login/HelloApplication.java
@@ -12,8 +12,8 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("view/hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 500, 500);
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("view/test.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
 //        scene.getStylesheets().add(getClass().getResource("/style/dashboard_css.css").toExternalForm());
         stage.setTitle("AHHHHH");
 //        stage.getIcons().add(new Image(""));
